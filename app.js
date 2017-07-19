d5d5439759c21f2499be67aa8ac2b0fe13696027
Wade Wegner <wade.wegner@gmail.com>
2017-07-19 08:43:13 -0700

    Functional prototype

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const sleep = require('sleep');
+var cookieParser = require('cookie-parser')
+const {
+  exec
+} = require('child_process');
+const oauth2 = require('salesforce-oauth2');
+
+const callbackUrl = process.env.CALLBACKURL;
+const consumerKey = process.env.CONSUMERKEY;
+const consumerSecret = process.env.CONSUMERSECRET;
 
 app.use('/scripts', express.static(__dirname + '/scripts'));
 
@@ -9,6 +19,8 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 app.set('view engine', 'ejs');
+
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
   res.render('pages/index', {});
@@ -20,9 +32,23 @@ app.get('/about', function (req, res) {
 
 app.get('/deploy', function (req, res) {
   const template = req.query.template;
-  res.render('pages/deploy', {
-    template: template
-  });
+
+  res.cookie('template', template);
+
+  const access_token = req.cookies.access_token;
+  const instance_url = req.cookies.instance_url;
+  const user_name = req.cookies.user_name;
+
+  if (access_token && instance_url) {
+    res.render('pages/deploy', {
+      template: template,
+      access_token: access_token,
+      instance_url: instance_url,
+      user_name: user_name
+    });
+  } else {
+    return res.redirect('/login');
+  }
 });
 
 app.get('/deploying', function (req, res) {
@@ -32,8 +58,115 @@ app.get('/deploying', function (req, res) {
   });
 });
 
-var port = process.env.PORT || 8080;
+app.get("/login", function (req, res) {
+  var uri = oauth2.getAuthorizationUrl({
+    redirect_uri: callbackUrl,
+    client_id: consumerKey,
+    scope: 'id api',
+    state: 'test123'
+    // You can change loginUrl to connect to sandbox or prerelease env.
+    //base_url: 'https://test.my.salesforce.com'
+  });
+  return res.redirect(uri);
+});
 
+app.get('/logout', function (req, res) {
+  res.clearCookie('access_token');
+  res.clearCookie('instance_url');
+
+  return res.redirect('/');
+});
+
+app.get('/oauth/callback', function (req, res) {
+  const authorizationCode = req.param('code');
+
+  oauth2.authenticate({
+    redirect_uri: callbackUrl,
+    client_id: consumerKey,
+    client_secret: consumerSecret,
+    code: authorizationCode
+  }, function (error, payload) {
+
+    res.cookie('access_token', payload.access_token);
+    res.cookie('instance_url', payload.instance_url);
+    res.cookie('user_name', payload.id);
+    
+    console.log(payload);
+
+    const template = req.cookies.template;
+    return res.redirect(`/deploy?template=${template}`);
+
+  });
+});
+
+
+var router = express.Router();
+
+router.post('/deploying', function (req, res) {
+
+  var command = req.body.command;
+  var param = req.body.param;
+
+  if (command === 'clone') {
+
+    exec(`cd /tmp;mkdir test;cd test;git clone ${param} repo`, (err, stdout, stderr) => {
+      res.json({
+        message: `Successfully cloned ${param}`
+      });
+    });
+
+  }
+
+  if (command === 'create') {
+    exec(`cd /tmp/test/repo;export FORCE_SHOW_SPINNER=;sfdx force:org:create -s -f ${param}`, (err, stdout, stderr) => {
+      res.json({
+        message: `Created scratch org: ${stdout}`
+      });
+    });
+  }
+
+  if (command === 'push') {
+    exec(`cd /tmp/test/repo;export FORCE_SHOW_SPINNER=;sfdx force:source:push`, (err, stdout, stderr) => {
+      res.json({
+        message: `Pushed source:\n\t${stdout}`
+      });
+    });
+  }
+
+  if (command === 'test') {
+    exec(`cd /tmp/test/repo;export FORCE_SHOW_SPINNER=;sfdx force:apex:test:run -r human --json | jq -r .result | jq -r .summary | jq -r .outcome`, (err, stdout, stderr) => {
+      res.json({
+        message: `Apex tests: ${stdout}`
+      });
+    });
+  }
+
+  if (command === 'url') {
+    const commandScript = 'cd /tmp/test/repo;export FORCE_SHOW_SPINNER=;echo $(sfdx force:org:display --json | jq -r .result | jq -r .instanceUrl)"/secur/frontdoor.jsp?sid="$(sfdx force:org:display --json | jq -r .result | jq -r .accessToken)';
+
+    exec(commandScript, (err, stdout, stderr) => {
+      console.log('stdout', stdout);
+      res.json({
+        message: `${stdout}`
+      });
+    });
+  }
+
+  if (command === 'clean') {
+
+    const commandScript = 'rm -rf /tmp/test';
+    exec(commandScript, (err, stdout, stderr) => {
+      res.json({
+        message: `Removed temp files and cleaned up`
+      });
+    });
+  }
+
+});
+
+app.use('/api', router);
+
+var port = process.env.PORT || 8080;
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`);
 });
--- scripts/deploying.js
+++ scripts/deploying.js
@@ -1,5 +1,145 @@
 $(document).ready(function () {
 
-  
+  let actionCount = 0;
+  let message = '';
 
+  function update_status(newMessage) {
+    actionCount += 1;
+    newMessage = newMessage.replace(/^\s+|\s+$/g, '');
+    message = `${actionCount}) ${newMessage}\n${message}`;
+    $('textarea#status').val(message);
+  }
+
+  let githubRepo = $('input#template').val();
+
+  let yamlFile = githubRepo.replace('github.com', 'raw.githubusercontent.com');
+  yamlFile += '/master/.salesforcedx.yaml';
+
+  update_status(`Checking for ${yamlFile}`);
+
+  $.ajax({
+    url: yamlFile,
+    type: 'GET',
+    success: (yamlFileDataResponse, status) => {
+
+      update_status(`Discovered ${yamlFile}`);
+
+      var doc = jsyaml.load(yamlFileDataResponse);
+
+      const assignPermset = doc['assign-permset'];
+      const deleteScratchOrg = doc['delete-scratch-org'];
+      const runApexTests = doc['run-apex-tests'];
+      const scratchOrgDef = doc['scratch-org-def'];
+      const showScratchOrgUrl = doc['show-scratch-org-url'];
+
+      update_status(`Parsed the following values from the yaml file:
+\tassign-permset: ${assignPermset}
+\tdelete-scratch-org: ${deleteScratchOrg}
+\trun-apex-tests: ${runApexTests}
+\tscratch-org-def: ${scratchOrgDef}
+\tshow-scratch-org-url: ${showScratchOrgUrl}`);
+
+      // clone org
+      let commandData = {};
+      commandData.command = 'clone';
+      commandData.param = githubRepo;
+
+      $.ajax({
+        type: 'POST',
+        url: '/api/deploying',
+        data: JSON.stringify(commandData),
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+        success: (commandDataResponse) => {
+          update_status(`${commandDataResponse.message}`);
+
+          // create org
+          commandData = {};
+          commandData.command = 'create';
+          commandData.param = scratchOrgDef;
+
+          $.ajax({
+            type: 'POST',
+            url: '/api/deploying',
+            data: JSON.stringify(commandData),
+            contentType: 'application/json; charset=utf-8',
+            dataType: 'json',
+            success: (commandDataResponse) => {
+              update_status(`${commandDataResponse.message}`);
+
+              // push source
+              commandData = {};
+              commandData.command = 'push';
+
+              $.ajax({
+                type: 'POST',
+                url: '/api/deploying',
+                data: JSON.stringify(commandData),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: (commandDataResponse) => {
+                  update_status(`${commandDataResponse.message}`);
+
+                  // run tests
+                  commandData = {};
+                  commandData.command = 'test';
+
+                  $.ajax({
+                    type: 'POST',
+                    url: '/api/deploying',
+                    data: JSON.stringify(commandData),
+                    contentType: 'application/json; charset=utf-8',
+                    dataType: 'json',
+                    success: (commandDataResponse) => {
+                      update_status(`${commandDataResponse.message}`);
+
+                      // generate url
+                      commandData = {};
+                      commandData.command = 'url';
+
+                      $.ajax({
+                        type: 'POST',
+                        url: '/api/deploying',
+                        data: JSON.stringify(commandData),
+                        contentType: 'application/json; charset=utf-8',
+                        dataType: 'json',
+                        success: (commandDataResponse) => {
+                          update_status(`Login URL: ${commandDataResponse.message}`);
+
+                          const url = commandDataResponse.message;
+                          $("#loginUrl").attr("href", url);
+                          $("#loginUrl").text(`${url.substring(0, 80)}...`);
+
+                          $('#loginBlock').show();
+
+                          // clean up
+                          commandData = {};
+                          commandData.command = 'clean';
+
+                          $.ajax({
+                            type: 'POST',
+                            url: '/api/deploying',
+                            data: JSON.stringify(commandData),
+                            contentType: 'application/json; charset=utf-8',
+                            dataType: 'json',
+                            success: (commandDataResponse) => {
+                              update_status(`${commandDataResponse.message}`);
+
+                              message = `DONE!\n\n${message}`;
+                              $('textarea#status').val(message);
+                            }
+                          });
+                        }
+                      });
+
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+  });
 });
