475780a7406ca5d039bbe9603a08301d249e8e4d
Wade Wegner <wade.wegner@gmail.com>
2017-08-02 09:18:56 -0700

    updated

--- app.js
+++ app.js
@@ -161,7 +161,7 @@ const router = express.Router();
 
 router.get('/test', (req, res) => {
 
-  const script = `ls -lAR /app/.local/`;
+  const script = `jq --help`;
 
   commands.run('test', script, (result) => {
     res.json({
