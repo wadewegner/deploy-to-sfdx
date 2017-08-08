970b8860a0152fbcf2d8edb2c7ca1d8f7a5e7d30
Ben Snyder <bsnyder@salesforce.com>
2017-08-08 16:59:37 -0400

    UX redesign, CSS build modifications, added SLDS and associated static assets

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ const consumerKey = process.env.CONSUMERKEY;
 const consumerSecret = process.env.CONSUMERSECRET;
 
 app.use('/scripts', express.static(`${__dirname}/scripts`));
+app.use('/public', express.static(`${__dirname}/public`));
 
 app.use(bodyParser.urlencoded({
   extended: true
