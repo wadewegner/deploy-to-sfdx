94732051d0120f57c59396da8a2931451f29cf89
Wade Wegner <wade.wegner@gmail.com>
2017-08-22 10:30:26 -0700

    Refactored to support repos w/o .yaml file

--- scripts/deploying.js
+++ scripts/deploying.js
@@ -29,94 +29,119 @@ $(document).ready(() => {
     });
   }
 
+  function deploy(yamlSettings, githubRepo) {
+
+    const timestamp = new Date().getTime().toString();
+
+    return deployingApi('clone', timestamp, githubRepo)
+      .then(() => {
+        return deployingApi('create', timestamp, yamlSettings.scratchOrgDef);
+      })
+      .then(() => {
+        return deployingApi('push', timestamp);
+      })
+      .then(() => {
+        if (yamlSettings.permsetName) {
+          return deployingApi('permset', timestamp, yamlSettings.permsetName);
+        } else {
+          return null;
+        }
+      })
+      .then(() => {
+        return deployingApi('test', timestamp, yamlSettings.runApexTests);
+      })
+      .then(() => {
+
+        // generate url
+        let commandData = {};
+        commandData.command = 'url';
+        commandData.timestamp = timestamp;
+
+        return $.ajax({
+          type: 'POST',
+          url: '/api/deploying',
+          data: JSON.stringify(commandData),
+          contentType: 'application/json; charset=utf-8',
+          dataType: 'json',
+          success: (commandDataResponse) => {
+            update_status(`Generated a login url: ${commandDataResponse.message}`);
+
+            const url = commandDataResponse.message;
+
+            $('#loginUrl').attr('href', url);
+            $('#loginUrl').text(`${url.substring(0, 80)}...`);
+            $('#loginBlock').show();
+            $('div#loaderBlock').hide();
+
+            // clean up
+            commandData = {};
+            commandData.command = 'clean';
+            commandData.timestamp = timestamp;
+          }
+        }).then(() => {
+          return deployingApi('clean', timestamp)
+            .then(() => {
+
+              message = `Finished. You have deployed the app to your scratch org!\n\n${message}`;
+              $('textarea#status').val(message);
+
+            });
+        });
+      });
+  }
+
   const githubRepo = $('input#template').val();
   let yamlFile = githubRepo.replace('github.com', 'raw.githubusercontent.com');
   yamlFile += '/master/.salesforcedx.yaml';
 
+  const yamlSettings = {};
+
   $.ajax({
     url: yamlFile,
     type: 'GET',
+    error: (XMLHttpRequest, textStatus, errorThrown) => {
+
+      yamlSettings.assignPermset = 'false';
+      yamlSettings.permsetName = '';
+      yamlSettings.deleteScratchOrg = 'false';
+      yamlSettings.runApexTests = 'false';
+      yamlSettings.scratchOrgDef = 'config/project-scratch-def.json';
+      yamlSettings.showScratchOrgUrl = 'true';
+
+      update_status(`Didn't find a .salesforcedx.yaml file. Using defaults:
+\tassign-permset: ${yamlSettings.assignPermset}
+\tpermset-name: ${yamlSettings.permsetName}
+\tdelete-scratch-org: ${yamlSettings.deleteScratchOrg}
+\trun-apex-tests: ${yamlSettings.runApexTests}
+\tscratch-org-def: ${yamlSettings.scratchOrgDef}
+\tshow-scratch-org-url: ${yamlSettings.showScratchOrgUrl}`);
+
+      deploy(yamlSettings, githubRepo);
+
+    },
     success: (yamlFileDataResponse, status) => {
 
       update_status(`Discovered ${yamlFile}`);
 
       const doc = jsyaml.load(yamlFileDataResponse);
 
-      const timestamp = new Date().getTime().toString();
-
-      const assignPermset = doc['assign-permset'];
-      const permsetName = doc['permset-name'];
-      const deleteScratchOrg = doc['delete-scratch-org'];
-      const runApexTests = doc['run-apex-tests'];
-      const scratchOrgDef = doc['scratch-org-def'];
-      const showScratchOrgUrl = doc['show-scratch-org-url'];
+      yamlSettings.assignPermset = doc['assign-permset'];
+      yamlSettings.permsetName = doc['permset-name'];
+      yamlSettings.deleteScratchOrg = doc['delete-scratch-org'];
+      yamlSettings.runApexTests = doc['run-apex-tests'];
+      yamlSettings.scratchOrgDef = doc['scratch-org-def'];
+      yamlSettings.showScratchOrgUrl = doc['show-scratch-org-url'];
 
       update_status(`Parsed the following values from the yaml file:
-\tassign-permset: ${assignPermset}
-\tpermset-name: ${permsetName}
-\tdelete-scratch-org: ${deleteScratchOrg}
-\trun-apex-tests: ${runApexTests}
-\tscratch-org-def: ${scratchOrgDef}
-\tshow-scratch-org-url: ${showScratchOrgUrl}`);
+\tassign-permset: ${yamlSettings.assignPermset}
+\tpermset-name: ${yamlSettings.permsetName}
+\tdelete-scratch-org: ${yamlSettings.deleteScratchOrg}
+\trun-apex-tests: ${yamlSettings.runApexTests}
+\tscratch-org-def: ${yamlSettings.scratchOrgDef}
+\tshow-scratch-org-url: ${yamlSettings.showScratchOrgUrl}`);
 
-      return deployingApi('clone', timestamp, githubRepo)
-        // .then(() => {
-        //   return deployingApi('auth', timestamp);
-        // })
-        .then(() => {
-          return deployingApi('create', timestamp, scratchOrgDef);
-        })
-        .then(() => {
-          return deployingApi('push', timestamp);
-        })
-        .then(() => {
-          if (permsetName) {
-            return deployingApi('permset', timestamp, permsetName);
-          } else {
-            return null;
-          }
-        })
-        .then(() => {
-          return deployingApi('test', timestamp, runApexTests);
-        })
-        .then(() => {
+      deploy(yamlSettings, githubRepo);
 
-          // generate url
-          let commandData = {};
-          commandData.command = 'url';
-          commandData.timestamp = timestamp;
-
-          return $.ajax({
-            type: 'POST',
-            url: '/api/deploying',
-            data: JSON.stringify(commandData),
-            contentType: 'application/json; charset=utf-8',
-            dataType: 'json',
-            success: (commandDataResponse) => {
-              update_status(`Generated a login url: ${commandDataResponse.message}`);
-
-              const url = commandDataResponse.message;
-
-              $('#loginUrl').attr('href', url);
-              $('#loginUrl').text(`${url.substring(0, 80)}...`);
-              $('#loginBlock').show();
-              $('div#loaderBlock').hide();
-
-              // clean up
-              commandData = {};
-              commandData.command = 'clean';
-              commandData.timestamp = timestamp;
-            }
-          }).then(() => {
-            return deployingApi('clean', timestamp)
-              .then(() => {
-
-                message = `Finished. You have deployed the app to your scratch org!\n\n${message}`;
-                $('textarea#status').val(message);
-
-              });
-          });
-        });
     }
   });
 });
