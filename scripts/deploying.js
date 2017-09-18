64d3ba3e2684d07a381888b2ed51e3431d7c63f3
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2017-09-18 10:28:03 -0500

    ajax timeout shorter than sdfx wait?

--- scripts/deploying.js
+++ scripts/deploying.js
@@ -34,6 +34,7 @@ $(document).ready(() => {
       data: JSON.stringify(commandData),
       contentType: 'application/json; charset=utf-8',
       dataType: 'json',
+      timeout: 0,
       success: (commandDataResponse) => {
         update_status(`${commandDataResponse.message}`);
       },
